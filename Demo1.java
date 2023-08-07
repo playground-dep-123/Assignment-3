4920bb14e0f8f0af0b443bad12f6e2f3ce5605ed
Madusha Akalanka <madushaakalanka97@gmail.com>
2023-08-08 00:56:19 +0530

    Design Table

--- Demo1.java
+++ Demo1.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class Demo1{
     private final static Scanner scanner=new Scanner(System.in);
     public static void main(String[] args) {
@@ -28,8 +29,10 @@ public class Demo1{
         //Subject 1
         System.out.print("Enter Your Subject 1: ");
         String subject1=scanner.nextLine();
+        int codeSubject1=Integer.parseInt(subject1.substring(3));
 
-        if(subject1.startsWith("se-")==false)
+
+        if(subject1.startsWith("SE-")==false)
         {
             System.out.println("\033[31:1mInvalid Subject");
             return;
@@ -40,6 +43,8 @@ public class Demo1{
         double marks1=scanner.nextDouble();
         scanner.nextLine();
 
+
+
         if((0<=marks1 & 100>=marks1)==false){
             System.out.println("\033[31:1mInvalid Marks");
             return;
@@ -48,8 +53,9 @@ public class Demo1{
         //Subject 2
          System.out.print("Enter Your Subject 2: ");
         String subject2=scanner.nextLine();
+          int codeSubject2=Integer.parseInt(subject2.substring(3));
 
-        if((subject2.startsWith("se-") && subject2.equals(subject1)))
+        if((subject2.startsWith("SE-") && subject2.equals(subject1)))
         {
             System.out.println("\033[31:1mInvalid Subject");
             return;
@@ -67,8 +73,9 @@ public class Demo1{
 //Subject 3
          System.out.print("Enter Your Subject 3: ");
         String subject3=scanner.nextLine();
+          int codeSubject3=Integer.parseInt(subject3.substring(3));
 
-        if((subject2.startsWith("se-") && (subject3.equals(subject1) || subject3.equals(subject2))))
+        if((subject2.startsWith("SE-") && (subject3.equals(subject1) || subject3.equals(subject2))))
         {
             System.out.println("\033[31:1mInvalid Subject");
             return;
@@ -85,7 +92,47 @@ public class Demo1{
         }
 
 
-       
+       double totalMarks=marks1+marks2+marks3;
+       double avgMarks=totalMarks/3;
+
+
+
+
+String status;
+
+if(avgMarks>=75)status="Distinguish pass";
+
+else if(avgMarks>=65) status="Credit Pass";
+else if(avgMarks>=55) status="Pass";
+else status="Fail";
+
+String subject="Maths";
+
+final String colorRed="\033[31;1m";
+final String colorGreen="\033[32;1m";
+final String colorBlue="\033[34;1m";
+final String colorYellow="\033[38;1m";
+final String COLORRESET="\033[0m";
+final String COLOUR=colorBlue;
+
+
+    System.out.println("+"+"-".repeat(53)+"+");
+    System.out.printf("|Name   : \033[32;1m%-43s\033[0m |\n",name.toUpperCase());
+    System.out.printf("|Age    : \033[31:1m%d\033[0m %-39s  |\n",age,"Years old");
+    System.out.printf("|Status : %s%-41s\033[0m   |\n",colorBlue,status);
+    System.out.printf("|Total  : %-15.2f Avg:%.2f%-17s  |\n",totalMarks,avgMarks,"%");
+    System.out.println("+"+"-".repeat(53)+"+");
+
+    System.out.printf("|%-16s| %-15s |%-18s|\n","Subject","Marks","Status");
+    System.out.println("+"+"-".repeat(53)+"+");
+    System.out.printf("|SE-%-16s| %-15.2f |%s%-18s%s|\n",subject,marks1,COLOUR,status,COLORRESET);
+    System.out.printf("|%-16s| %-15.2f |%s%-18s%s|\n",subject,marks1,COLOUR,status,COLORRESET);
+        System.out.printf("|%-16s| %-15.2f |%s%-18s%s|\n",subject,marks1,COLOUR,status,COLORRESET);
+     System.out.println("+"+"-".repeat(53)+"+");
+
+
+
+
 
 
 
